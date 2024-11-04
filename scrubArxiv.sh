ec6125eebdc0c52ff45a78d0d9da4b1e0acd056a
Lorenzo <lorenzo@DESKTOP-L6EDCMR.>
2024-11-04 10:27:43 +0000

    Updated scrubArxivù

--- scrubArxiv.sh
+++ scrubArxiv.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-RESULT_DIR="/path/to/Arxiv_Daily"
+RESULT_DIR="/home/lorenzo/Desktop/Arxiv_Daily"
 TODAY=$(date +%Y_%m_%d)
 FILENAME="${RESULT_DIR}/${TODAY}_Arxiv.txt"
 
 # Check if today is Saturday (6) or Sunday (7)
 DAY_OF_WEEK=$(date +%u)
 
-if [ "$DAY_OF_WEEK" -ge 6 ]; then\
+if [ "$DAY_OF_WEEK" -ge 6 ]; then
   exit 0
 fi
 
@@ -15,16 +15,11 @@ if [ -f "$FILENAME" ]; then
   exit 0
 fi
 
-# Check if today is the first day of the month
-DAY_OF_MONTH=$(date +%d)
+MOST_RECENT_FILE=$(ls -1 "${RESULT_DIR}"/*_Arxiv.txt 2>/dev/null | sort -V | tail -n 1)
+PREVIOUS_MONTH=$(basename "$MOST_RECENT_FILE" | cut -d'_' -f1,2)
+CURRENT_MONTH=$(date +%Y_%m)
 
-if { [ "$DAY_OF_MONTH" -eq 1 ] && [ "$DAY_OF_WEEK" -lt 6 ]; } || \
-   { [ "$DAY_OF_MONTH" -eq 2 ] && [ "$(date -d 'yesterday' +%u)" -ge 6 ]; } || \
-   { [ "$DAY_OF_MONTH" -eq 3 ] && [ "$(date -d '2 days ago' +%u)" -ge 6 ] && [ "$(date -d 'yesterday' +%u)" -ge 6 ]; }; then
-
-  # Get the previous month and year
-  PREVIOUS_MONTH=$(date -d '1 month ago' +%Y_%m)
-
+if [ "$CURRENT_MONTH" != "$PREVIOUS_MONTH" ]; then
   # Create the previous month directory if it doesn't exist
   PREVIOUS_MONTH_DIR="${RESULT_DIR}/${PREVIOUS_MONTH}"
   if [ ! -d "$PREVIOUS_MONTH_DIR" ]; then
@@ -35,4 +30,4 @@ if { [ "$DAY_OF_MONTH" -eq 1 ] && [ "$DAY_OF_WEEK" -lt 6 ]; } || \
   find "$RESULT_DIR" -maxdepth 1 -type f -name "${PREVIOUS_MONTH}_*_Arxiv.txt" -exec mv {} "$PREVIOUS_MONTH_DIR" \;
 fi
 
-python ./check_arxiv.py
+python3 /home/lorenzo/phd/VariousCodes/ArxivBot/check_arxiv.py
